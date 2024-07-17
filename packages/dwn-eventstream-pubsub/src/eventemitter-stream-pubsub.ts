823ed221852c458bed283166e7d05233bf9d50cb
David Mehi <davidmehi@google.com>
2024-07-17 13:11:52 +0000

    Removed extra console.log statements in eventemitter-stream-pubsub.ts

--- packages/dwn-eventstream-pubsub/src/eventemitter-stream-pubsub.ts
+++ packages/dwn-eventstream-pubsub/src/eventemitter-stream-pubsub.ts
@@ -25,9 +25,7 @@ export interface EventEmitterStreamConfig {
 
 
 export class EventEmitterStream implements EventStream {
-  //private eventEmitter: EventEmitter;
   isOpen: boolean = false;
-  //pubSubClient;
   projectId: string;
 
   constructor(config: EventEmitterStreamConfig = {}) {
@@ -43,14 +41,7 @@ export class EventEmitterStream implements EventStream {
       this.errorHandler = config.errorHandler;
     }
 
-    //this.eventEmitter.on('error', this.errorHandler);
-
-    // Instantiates a client
-    //this.projectId = 'gcda-tbd-dwn';
     this.projectId = process.env.DWN_PROJECT_ID as string;
-    //console.log("Instantiates PubSub client: " + projectId);
-    //this.pubSubClient = new PubSub({projectId});
-    //console.log("PubSub client created");
   }
 
   /**
@@ -60,20 +51,12 @@ export class EventEmitterStream implements EventStream {
 
   
   async subscribe(tenant: string, id: string, listener: EventListener): Promise<EventSubscription> {
-    
-    console.log("subscribe");
 
     // Get topic name
     let topicName = this.getTopicName(tenant);
 
     // Get subscription name
     let subscriptionName = this.getSubscriptionName(tenant, id);
-
-    // Create a subscription
-    //console.log(`Creating subscription.`);
-    //const [pubSubTopic] = await this.pubSubClient.createTopic(topicName);
-    //const [subscription] = await pubSubTopic.createSubscription(subscriptionName);
-    //console.log(`Subscription ${subscriptionName} created.`);
 
     if (!this.projectId) {
       const err = new Error("Error: project id is not set.  Check env variable DWN_PROJECT_ID");
@@ -86,20 +69,19 @@ export class EventEmitterStream implements EventStream {
     let topicExists: any;
     try { 
       [topicExists] = await pubSubTopic.exists();
-      console.log([topicExists]);
     } catch(e) {
-      console.log("Error topic exists");
+      console.log("Error topic exists: " + topicName);
     }
 
     if(!topicExists) {
       try{
         [pubSubTopic] =  await pubSubClient.createTopic(topicName);
-        console.log(`Topic created ${topicName}`);
+        //console.log(`Topic created ${topicName}`);
       } catch(e) {
-        console.log("Error creating topic");
+        console.log("Error creating topic: " + topicName);
       }
     } else {
-        console.log(`Topic exists ${topicName}`);
+        //console.log(`Topic exists ${topicName}`);
     }
 
     let subscription = await pubSubTopic.subscription(subscriptionName);
@@ -109,21 +91,16 @@ export class EventEmitterStream implements EventStream {
     if(!subscriptionExists) {
       try {
         [subscription] = await pubSubTopic.createSubscription(subscriptionName)
-        console.log(`Subscription created ${subscriptionName}`);
+        //console.log(`Subscription created ${subscriptionName}`);
       } catch(e) {
-        console.log("Error creating subscription");
+        console.log("Error creating subscription: " + subscriptionName);
       }
     } else {
-        console.log(`Subscription exists ${subscriptionName}`);
+        //console.log(`Subscription exists ${subscriptionName}`);
     }
 
     // Create Message Handler
-    //let subscriptionMessageHandler = new SubscriptionMessageHandler(tenant, subscriptionName, listener, this.pubSubClient);
-    //subscription.on('message', async subscriptionMessageHandler.messageHandler);
     subscription.on('message', async (message) => {
-      console.log(`Received message on: ${message.id}:`);
-      console.log(`\tData: ${message.data.toString()}`);
-      console.log(`\tAttributes: ${message.attributes}`);
   
       // "Ack" (acknowledge receipt of) the message
       message.ack();
@@ -131,32 +108,10 @@ export class EventEmitterStream implements EventStream {
       // Decompose message to objects
   
       const messageObject = JSON.parse(message.data.toString());
-      /*{
-        tenant: tenant,
-        event: event,
-        indexes: indexes
-      } */
-      console.log("tenant: " + JSON.stringify(messageObject.tenant));
-      console.log("event: " + JSON.stringify(messageObject.event));
-      console.log("indexes: " + JSON.stringify(messageObject.indexes));
   
       // Need to link this back to the listener
-      console.log("listener (on): " + listener);
       listener(tenant,messageObject.event,messageObject.indexes);
-      //const response = await message.ackWithResponse();
     });
-
-    //subscription.on('debug', msg => { console.log("debug: " + msg.message); });
-    /*
-    subscription.on('close', () => {
-      console.log("removing listener (on close): " + subscription.name); 
-      // gets the listeners for the message
-      //for (const listener of subscription.listeners('message')) {
-      //  console.log("listener (close): " + JSON.stringify(listener));
-      //}
-      subscription.removeListener('message', listener);
-      subscription.delete();
-    }); */
 
     //this.eventEmitter.on(`${tenant}_${EVENTS_LISTENER_CHANNEL}`, listener);
     // needs to close this subscription
@@ -164,7 +119,6 @@ export class EventEmitterStream implements EventStream {
     return {
       id,
       close: async (): Promise<void> => { 
-        console.log("closing subscription (return): " + subscription.name); 
         const [subscriptionToCloseExists] = await subscription.exists();
         if(subscriptionToCloseExists) {
           try {
@@ -172,10 +126,10 @@ export class EventEmitterStream implements EventStream {
             await subscription.close(); 
             await subscription.delete(); 
           } catch(e) {
-            console.log("Error closing and deleting subscription");
+            console.log("Error closing and deleting subscription: " + subscription.name);
           }
         } else {
-          console.log("Subscription " + subscription.name + " already closed");
+          //console.log("Subscription " + subscription.name + " already closed");
         }
       }
     };
@@ -196,34 +150,30 @@ export class EventEmitterStream implements EventStream {
 
   async open(): Promise<void> {
     this.isOpen = true;
-    console.log("Opening PubSub");
+    //console.log("Opening PubSub");
   }
 
   async close(): Promise<void> {
     this.isOpen = false;
-    console.log("Closing PubSub");
+    //console.log("Closing PubSub");
     //this.eventEmitter.removeAllListeners();
     //await this.pubSubClient.subscription(this.getSubscriptionName()).removeListener('message', this.messageHandler);
     // close all subscriptions for the pubsub client
     // Lists all subscriptions in the current project
     let pubSubClient = new PubSub({projectId: this.projectId});
     const [subscriptions] = await pubSubClient.getSubscriptions();
-    console.log('Number of subscriptions:' + subscriptions.length);
     //subscriptions.forEach(subscription => { "Closing:" + console.log(subscription.name);  subscription.close(); } );
     for (const subscription of subscriptions) {
       try { 
-        console.log("Closing " + subscription.name);
         await subscription.close();
         //await subscription.delete();
       } catch(e) {
-        console.log("Error closing and deleting subscription");
+        //console.log("Error closing and deleting subscription");
       }
     }
   }
 
   async emit(tenant: string, event: MessageEvent, indexes: KeyValues): Promise<void> {
-    
-    console.log("emit");
 
     if (!this.isOpen) {
       this.errorHandler(new DwnError(
@@ -232,11 +182,7 @@ export class EventEmitterStream implements EventStream {
       ));
       return;
     }
-    //this.eventEmitter.emit(`${tenant}_${EVENTS_LISTENER_CHANNEL}`, tenant, event, indexes);
 
-    //console.log("tenant:" + tenant);
-    //console.log("event:" +  JSON.stringify(event));
-    //console.log("indexes:" +  JSON.stringify(indexes));
     // PubSub Topic name
     let topicName = this.getTopicName(tenant);
 
@@ -246,9 +192,7 @@ export class EventEmitterStream implements EventStream {
       indexes: indexes
     }
 
-    
     let message = JSON.stringify(messageObject);
-    console.log(message);
 
     let pubSubClient = new PubSub({projectId: this.projectId});
     let pubSubTopic = await pubSubClient.topic(topicName);
@@ -259,17 +203,16 @@ export class EventEmitterStream implements EventStream {
 
       try { 
         [pubSubTopic] =  await pubSubClient.createTopic(topicName);
-        console.log(`Topic created ${topicName}`);
+        //console.log(`Topic created ${topicName}`);
       } catch(e) {
-        console.log("Error creating topic");
+        console.log("Error creating topic: " + topicName);
       }
 
     } else {
-        console.log(`Topic exists ${topicName}`);
+        //console.log(`Topic exists ${topicName}`);
     }
 
     const messageId = pubSubTopic.publishMessage({data: Buffer.from(message)});
-    console.log("message published: " + messageId);
 
   }
 }
